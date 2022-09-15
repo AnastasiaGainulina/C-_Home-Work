b25a31ebf823ac7bb225774566f8ca971f9bc66c
Anastasia Gainulina <Famousnastya@gmail.com>
2022-09-15 23:17:06 +0300

    Ready

--- Ex_02/Program.cs
+++ Ex_02/Program.cs
@@ -3,3 +3,17 @@ Console.WriteLine("Введите число a ");
 
 int a = int.Parse(Console.ReadLine());
 
+Console.WriteLine("Введите число b ");
+
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число c ");
+
+int c = int.Parse(Console.ReadLine());
+
+int max = a; 
+if (b>max) max = b;
+if (c>max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
