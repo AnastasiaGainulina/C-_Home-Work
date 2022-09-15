5b525b66da57f62d829b0fdc4f8b4a65868a5a89
Anastasia Gainulina <Famousnastya@gmail.com>
2022-09-15 22:50:02 +0300

    Changed number

--- Ex_01/Program.cs
+++ Ex_01/Program.cs
@@ -1,9 +1,9 @@
 ﻿//  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-Console.WriteLine("Введите число №1 ");
+Console.WriteLine("Введите число a ");
 
 int a = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Введите число №2 ");
+Console.WriteLine("Введите число b ");
 
 int b = int.Parse(Console.ReadLine());
 
